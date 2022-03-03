8e99c8422a35513b112a804d49cbf5d0fa93f41f
AnassELYO <anass69007@gmail.com>
2022-03-03 12:10:21 +0100

    validation errors

--- resources/views/create_star.blade.php
+++ resources/views/create_star.blade.php
@@ -12,19 +12,31 @@
                     <input type="hidden" name="id">
                     <div class="mb-3 flex  items-center justify-center">
                         <label for="name" class="mr-6 lg:mr-12">Name:</label>
-                        <input type="text" class="h-8" name="name" required>
+                        <input type="text" class="h-8 @error('name') is-invalid @enderror" name="name" required>
+                        @error('name')
+                        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="mb-3 flex items-center justify-center">
                         <label for="first_name" class="mr-6 lg:mr-12">First name:</label>
-                        <input type="text" class="h-8" name="first_name" required>
+                        <input type="text" class="h-8 @error('first_name') is-invalid @enderror" name="first_name" required>
+                        @error('first_name')
+                        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="mb-3 flex  items-center justify-center">
                         <label for="name" class="mr-6 lg:mr-12">Image:</label>
-                        <input type="url" class="h-8" name="image" required>
+                        <input type="url" class="h-8 @error('image') is-invalid @enderror" name="image" required>
+                        @error('image')
+                        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="mb-3 flex flex-col items-center justify-center">
                         <label for="name" class="mr-6 lg:mr-12">Description:</label>
-                        <textarea type="text" class="h-32 lg:h-40 lg:w-96" name="description" required></textarea>
+                        <textarea type="text" class="h-32 lg:h-40 lg:w-96 @error('description') is-invalid @enderror" name="description" required></textarea>
+                        @error('description')
+                        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">{{ $message }}</div>
+                        @enderror
                     </div>
                     @method('POST')
                     @csrf
